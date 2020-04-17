8c9a1a4ee8e253f0923015c7cf479434365c7af6
Archoe3DProd <davet-philippe@bluewin.ch>
2020-04-17 14:42:03 +0200

    Build a route, action and view, task 2

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'styles/atoms'
   get 'styles/molecules'
   get 'styles/organisms'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/ideas/index'
 end
