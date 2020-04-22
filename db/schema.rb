97876164a8a2b0e8fe17dfe25526113063f8a1af
Archoe3DProd <davet-philippe@bluewin.ch>
2020-04-22 19:27:56 +0200

    12. Extending an existing model, task 5

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_22_165614) do
+ActiveRecord::Schema.define(version: 2020_04_22_172726) do
 
   create_table "ideas", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "done_count"
+    t.string "photo_url"
   end
 
   create_table "items", force: :cascade do |t|
