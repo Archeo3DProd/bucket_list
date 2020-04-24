3ccf76c3acf8b848638f8673a854cc4f3ccfefd3
Archoe3DProd <davet-philippe@bluewin.ch>
2020-04-24 17:59:02 +0200

    06. Testing views with Capybara, task 5

--- test/system/simple_navigations_test.rb
+++ test/system/simple_navigations_test.rb
@@ -2,6 +2,10 @@ require "application_system_test_case"
 
 class SimpleNavigationsTest < ApplicationSystemTestCase
   test "search term is displayed" do
-    
+
+  end
+
+  test "style guide navigation" do
+
   end
 end
