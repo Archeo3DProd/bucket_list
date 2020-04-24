252c2603c29a6333fd8475e830d854574f38cd1c
Archoe3DProd <davet-philippe@bluewin.ch>
2020-04-24 20:09:39 +0200

    08. Testing dynamic values, task 2

--- test/system/ideas_test.rb
+++ test/system/ideas_test.rb
@@ -1,9 +1,17 @@
 require "application_system_test_case"
 
 class IdeasTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit ideas_url
-  #
-  #   assert_selector "h1", text: "Ideas"
-  # end
+
+  test 'create new idea' do
+
+    visit ('ideas/new')
+    fill_in 'title', with: 'Visit the Hermitage Museum'
+    fill_in 'done_count', with: 6164
+    click_on 'Create idea'
+    visit ('account/ideas')
+    assert page.has_content?('Visit the Hermitage Museum')
+    sleep(3.seconds)
+
+  end
+
 end
