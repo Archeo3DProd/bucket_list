4e88dcef23e47cb0c892345a72d24120208776dc
Archoe3DProd <davet-philippe@bluewin.ch>
2020-04-24 19:15:23 +0200

    06. Testing views with Capybara, task 6

--- test/system/simple_navigations_test.rb
+++ test/system/simple_navigations_test.rb
@@ -2,10 +2,30 @@ require "application_system_test_case"
 
 class SimpleNavigationsTest < ApplicationSystemTestCase
   test "search term is displayed" do
-
+    visit('/')
+    assert page.has_content?('Bucket List')
+    fill_in('q', with: 'Spain').send_keys(:enter)
+    assert has_content?('Spain')
+    assert current_url.include?('q=Spain')
   end
 
   test "style guide navigation" do
-
+    visit('/')
+    sleep(2.seconds)
+    click_on('Styleguide', match: :first)
+    sleep(2.seconds)
+    assert page.has_content?('Atoms')
+    assert page.has_content?('Molecules')
+    assert page.has_content?('Organisms')
+    click_on('Molecules', match: :first)
+    sleep(2.seconds)
+    assert page.has_content?('Card')
+    sleep(2.seconds)
+    click_on('Organisms', match: :first)
+    sleep(2.seconds)
+    assert page.has_content?('Create goal')
+    click_on('My Bucket List', match: :first)
+    assert page.has_content?('Some ideas')
+    sleep(2.seconds)
   end
 end
