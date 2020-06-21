753b4dbeaab9890d79e5dc02ac65841a615f2610
Archoe3DProd <davet-philippe@bluewin.ch>
2020-06-21 20:16:00 +0200

    14. Testing a model, task 4

--- test/models/idea_test.rb
+++ test/models/idea_test.rb
@@ -33,4 +33,14 @@ class IdeaTest < ActiveSupport::TestCase
     idea.save!
     refute_equal(idea.updated_at, first_updated_at)
   end
+
+    test 'updated_at is changed after updating done_count' do
+      idea = Idea.new
+      idea.done_count = 216
+      idea.save!
+      first_updated_at = idea.updated_at
+      idea.done_count = 95164
+      idea.save!
+      refute_equal(idea.updated_at, first_updated_at)
+    end
 end
