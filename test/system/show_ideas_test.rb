45df86a1dcf4329856be831eb3a020c470c8ab88
Archoe3DProd <davet-philippe@bluewin.ch>
2020-06-21 17:12:40 +0200

    11. Building the show idea view, task 8

--- test/system/show_ideas_test.rb
+++ test/system/show_ideas_test.rb
@@ -5,9 +5,16 @@ class ShowIdeasTest < ApplicationSystemTestCase
   test 'show displays title' do
     idea = Idea.new
     idea.title = 'Walking on a frozen lake'
+    idea.done_count = 216
     idea.save!
 
     visit(show_idea_path(idea))
     assert page.has_content?('Walking on a frozen lake')
+    assert page.has_content?('216 have done')
+    assert page.has_content?(idea.created_at.strftime("%d %b '%y"))
+
+    click_on('Edit')
+
+    assert_equal current_path, edit_idea_path(idea)
   end
 end
