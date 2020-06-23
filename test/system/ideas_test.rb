fcc4c44a5d3bfca84d23401d33aa1172383dbc13
Archoe3DProd <davet-philippe@bluewin.ch>
2020-06-23 11:27:07 +0200

    More complex filters, task 4

--- test/system/ideas_test.rb
+++ test/system/ideas_test.rb
@@ -70,4 +70,30 @@ class IdeasTest < ApplicationSystemTestCase
     click_on('Search', match: :first)
     assert page.has_content?('No ideas found !')
   end
+
+  test 'for matching title and/or description' do
+    idea_1 = Idea.new
+    idea_1.title = 'Go cycling across Europe'
+    idea_1.description = 'An amazing way to see lots of Europe'
+    idea_1.save!
+    idea_2 = Idea.new
+    idea_2.title = 'Visit Provence'
+    idea_2.description = 'Go to vineyards, go cycling up Mont Ventoux, see the fileds of lavander'
+    idea_2.save!
+    idea_3 = Idea.new
+    idea_3.title = 'Overnight hike in Switzerland'
+    idea_3.description = 'Stay in a Swiss refuge in the mountains'
+    idea_3.save!
+
+    visit(root_path)
+
+    fill_in('q', with: 'cycling')
+    click_on('Search', match: :first)
+
+    assert page.has_content?('Go cycling across Europe')
+
+    assert page.has_content?('Visit Provence')
+
+    refute page.has_content?('Overnight hike in Switzerland')
+  end
 end
